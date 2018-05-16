ec7302820045414fd33986fd2f8e723d8ef5a272
anshulsamar <asamar@stanford.edu>
2018-05-16 23:13:02 +0000

    Paths: add tensorboard dir

--- paths.py
+++ paths.py
@@ -9,12 +9,16 @@ np_dir = data_dir + "npy/uganda/" # output dir for process_data
 train_images = data_dir + "npy/train/" # contains train images
 test_images = data_dir + "npy/test/" # contains test images
 lsms_images = data_dir + "tif/uganda_lsms/" # contains lsms images
+lsms_images_big = data_dir + "tif/uganda_lsms_big/" # contains big images
 train_tiles = data_dir + "tiles/train/" # train tiles (triplets)
 test_tiles = data_dir + "tiles/test/" # test tiles (triplets)
 lsms_tiles = data_dir + "tiles/lsms/" # lsms tiles (triplets)
 
-# for figures
+# figure dir
 fig_dir = home_dir + "/figures/"
+
+# log dir
+log_dir = home_dir + "/runs/"
 
 # model dir
 model_dir = home_dir + "/models/"
