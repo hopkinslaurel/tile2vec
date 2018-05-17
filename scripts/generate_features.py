4b19459a50f11367b75cd19f7f38a1e2be64dfb5
anshulsamar <asamar@stanford.edu>
2018-05-17 05:31:14 +0000

    Scripts: removed core, added todo

--- scripts/generate_features.py
+++ scripts/generate_features.py
@@ -1,5 +1,7 @@
 # Based on Neal Jean/Sherrie's code
 
+import sys
+sys.path.append('/home/asamar/tile2vec/')
 import numpy as np
 import os
 import gdal
@@ -16,6 +18,16 @@ from torch.autograd import Variable
 from src.tilenet import make_tilenet
 from src.resnet import ResNet18
 from src.data_utils import clip_and_scale_image
+import argparse
+import paths
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_fn', dest='model_fn')
+parser.add_argument('-big', action='store_true')
+parser.add_argument('-small', action='store_true')
+parser.add_argument('-q', action='store_true')
+args = parser.parse_args()
+print(args)
 
 # Setting up model
 in_channels = 5
@@ -25,17 +37,13 @@ tilenet = make_tilenet(in_channels = in_channels, z_dim=z_dim)
 if cuda: tilenet.cuda()
 
 # Load parameters
-model_fn = '/home/asamar/tile2vec/models/5_bands/TileNet.ckpt'
-tilenet.load_state_dict(torch.load(model_fn))
+tilenet.load_state_dict(torch.load(args.model_fn))
 tilenet.eval()
 
 test_imgs = 642
 patches_per_img = 10
-X = np.zeros((test_imgs, z_dim))
 bands = 5
 
-for i in range(test_imgs):
-    img_name = '/home/asamar/tile2vec/data/uganda_landsat_test/landsat7_uganda_3yr_cluster_' + str(i) + '.tif'
-    X[i] = get_test_features (img_name, tilenet, z_dim, cuda, bands, patch_size=50, patch_per_img=10, save=True, verbose=True)
+# See code in run.py for remaining code to generate features
 
-np.save('/home/asamar/tile2vec/data/uganda_lsms/cluster_conv_features.npy', X)
+
