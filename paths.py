70220d78f147060f499333ce0b03dfdc5f137ac1
Anshul Samar <asamar@stanford.edu>
2018-06-06 17:30:20 +0000

    Paths: Updating paths

--- paths.py
+++ paths.py
@@ -6,14 +6,16 @@ tif_dir = data_dir + "tif/uganda_all/" # earth engine exports
 np_dir = data_dir + "npy/uganda/" # output dir for process_data
 
 # for make_triplets.py
+all_images = data_dir + "npy/all/" # contains all images
 train_images = data_dir + "npy/train/" # contains train images
 test_images = data_dir + "npy/test/" # contains test images
-lsms_images = data_dir + "tif/uganda_lsms/" # contains lsms images
+lsms_images_small = data_dir + "tif/uganda_lsms_small/" # contains lsms images
 lsms_images_big = data_dir + "tif/uganda_lsms_big/" # contains big images
 train_tiles = data_dir + "tiles/train/" # train tiles (triplets)
 val_tiles = data_dir + "tiles/val/" # val tiles (triplets)
 test_tiles = data_dir + "tiles/test/" # test tiles (triplets)
-lsms_tiles = data_dir + "tiles/lsms/" # lsms tiles (triplets)
+lsms_train_tiles = data_dir + "tiles/lsms_train/" # lsms tiles (triplets)
+lsms_val_tiles = data_dir + "tiles/lsms_val/" # lsms tiles (triplets)
 
 # figure dir
 fig_dir = home_dir + "/figures/"
