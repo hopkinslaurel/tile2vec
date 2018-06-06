ec929fa2aee40c1442ffaa9a11bf79c2b57d0c12
Anshul Samar <asamar@stanford.edu>
2018-06-06 17:46:56 +0000

    Uganda LSMS: readme

--- fig_utils.py
+++ fig_utils.py
@@ -1,17 +1,10 @@
 # fig_utils: predicting-poverty repo (author: Neal Jean)
-# copy pasted from fig_utils, with some edits - 
-# This is not fully tested/experimental -
+# copy pasted from fig_utils, removing unneeded parts
 # To re-create figure 4A of science paper, I added
 # Tile2Vec curve and manually put in PCA dimension of 10
-# for all X_tf data. Note that DHS will no longer
-# work. 
+# for all X_tf data. 
 # I am getting runtime warning with scipy stats when running this
 # w/ poverty_plot "invalid value encountered"
-# Normally the setup is that you generate conv_features.npy file
-# and that gets used. With predict_consumption you can now pass in
-# an experiment name to specify which conv_features file.
-# This needs to be updated for poverty curve plotting
-# which currently is not passing in experiment names.
 
 import numpy as np
 import pandas as pd
@@ -88,30 +81,25 @@ def compute_plot_params(y, y_hat, margin):
 
 def compare_models(
         country_path, survey, percentiles, dimension, k, k_inner, points, alpha_low, alpha_high, trials,
-        poverty_line, multiples):
+        poverty_line, multiples, exp=None):
     """
     Evaluates and plots comparison of transfer learning and nightlight models.
     """
     r2s, r2s_nl, r2s_tf = evaluate_percentiles(
-        country_path, survey, percentiles, dimension, k, k_inner, points, alpha_low, alpha_high, trials)
+        country_path, survey, percentiles, dimension, k, k_inner, points, alpha_low, alpha_high, trials, exp=exp)
     if survey == 'lsms':
         X, X_nl, X_tf, y = load_and_reduce_country_by_percentile(
-            country_path, survey, 1.0, dimension)
+            country_path, survey, 1.0, dimension, exp=exp)
         fractions = compute_fractions(poverty_line, multiples, y)
         plot_percentiles_lsms(percentiles, multiples, r2s, r2s_nl, r2s_tf, fractions)
-    elif survey == 'dhs':
-        plot_percentiles_dhs(percentiles, r2s, r2s_nl)
-
 
 def load_and_reduce_country_by_percentile(
-        country_path, survey, percentile, dimension):
+        country_path, survey, percentile, dimension, exp=None):
     """
     Loads data for one country up to a certain percentile.
     """
     if survey == 'lsms':
-        X, X_nl, X_tf, y = load_country_lsms(country_path)
-    elif survey == 'dhs':
-        X, X_nl, y = load_country_dhs(country_path)
+        X, X_nl, X_tf, y = load_country_lsms(country_path, exp=exp)
     X, X_nl, X_tf, y = threshold_by_percentile(X, X_nl, X_tf, y, percentile)
     X = reduce_dimension(X, dimension)
     X_tf = reduce_dimension(X, 10) #adding this extra step
@@ -131,7 +119,7 @@ def threshold_by_percentile(X, X_nl, X_tf, y, percentile):
 
 
 def evaluate_percentiles(
-        country_path, survey, percentiles, dimension, k, k_inner, points, alpha_low, alpha_high, trials):
+        country_path, survey, percentiles, dimension, k, k_inner, points, alpha_low, alpha_high, trials, exp=None):
     """
     Evaluate transfer learning and nightlight models for each percentile.
     """
@@ -141,7 +129,7 @@ def evaluate_percentiles(
     for idx, percentile in enumerate(percentiles):
         for trial in xrange(trials):
             X, X_nl, X_tf, y = load_and_reduce_country_by_percentile(
-                country_path, survey, percentile, dimension)
+                country_path, survey, percentile, dimension, exp=exp)
             _, r2, _ = run_cv(
                 X, y, k, k_inner, points, alpha_low, alpha_high,
                 randomize=False)
@@ -238,281 +226,6 @@ def plot_percentiles_lsms(percentiles, multiples, r2s, r2s_nl, r2s_tf, fractions
     plt.savefig('poverty_line.png')
     plt.close("all")
 
-def plot_percentiles_dhs(percentiles, r2s, r2s_nl):
-    """
-    Plots transfer learning model vs. nightlights model at each
-    """
-    sns.set_style('white')
-    plt.figure(figsize=(6, 6))
-    percentiles = [100 * x for x in percentiles]
-    plt.plot(percentiles, r2s)
-    plt.plot(percentiles, r2s_nl)
-    plt.legend(['Transfer learning', 'Nightlights'], loc='upper center')
-    plt.xlabel('Poorest percent of clusters used', fontsize=14)
-    plt.ylabel('$r^2$', fontsize=18)
-    plt.show()
-
-############################
-######## Figure 4cd ########
-############################
-
-
-def run_randomization_test(
-    country_names, country_paths, survey, dimension, k, k_inner, points,
-        alpha_low, alpha_high, trials):
-    """
-    Runs randomization test for a set of countries.
-    """
-    data = load_data(country_paths, survey, dimension)
-    true_r2s = compute_true_r2s(
-        data, k, k_inner, points, alpha_low, alpha_high)
-    shuffled_r2s = compute_shuffled_r2s(
-        data, k, k_inner, points, alpha_low, alpha_high, trials)
-    plot_shuffled_distributions(country_names, survey, shuffled_r2s, true_r2s)
-
-
-def compute_true_r2s(data, k, k_inner, points, alpha_low, alpha_high):
-    """
-    Uses data to compute true model r2s.
-    """
-    true_r2s = []
-    for (X, y) in data:
-        _, r2 = run_cv(X, y, k, k_inner, points, alpha_low, alpha_high)
-        true_r2s.append(r2)
-    return true_r2s
-
-
-def compute_shuffled_r2s(
-        data, k, k_inner, points, alpha_low, alpha_high, trials):
-    """
-    Uses data to compute shuffled model r2s.
-    """
-    shuffled_r2s = np.zeros((len(data), trials))
-    for data_idx, (X, y) in enumerate(data):
-        for trial in range(trials):
-            _, shuffled_r2s[data_idx, trial] = run_cv(
-                X, y, k, k_inner, points, alpha_low, alpha_high,
-                randomize=True)
-    return shuffled_r2s
-
-
-def plot_shuffled_distributions(country_names, survey, shuffled_r2s, true_r2s):
-    """
-    Plots shuffled r2 distributions vs. true model r2s.
-    """
-    colors = sns.color_palette('husl', len(country_names))
-    sns.set_style('white')
-    plt.figure(figsize=(8, 8))
-    frame = plt.gca()
-    for i in xrange(len(country_names)):
-        plt.axvline(
-            true_r2s[i], color=colors[i], linestyle='dashed', linewidth=2,
-            label=country_names[i].capitalize() + ': $r^2={0:.2f}$'.format(
-                true_r2s[i]))
-    plt.legend(
-        title='True {} Models:'.format(survey.upper()), loc='lower right',
-        bbox_to_anchor=(0.6, 0.45), fontsize=12)
-    for i in xrange(len(country_names)):
-        sns.kdeplot(shuffled_r2s[i, :], shade=True, color=colors[i])
-    plt.xlim((0, max(true_r2s) + 0.05))
-    plt.xlabel('$r^2$', fontsize=18)
-    plt.ylabel('Randomized $r^2$ distribution', fontsize=14)
-    frame.yaxis.set_ticklabels([])
-    plt.show()
-
-############################
-######### Figure 5 #########
-############################
-
-
-def evaluate_models(
-    country_names, country_paths, survey, dimension, k, trials, points,
-        alpha_low, alpha_high, cmap='Greens'):
-    """
-    Evaluates in- and out-of-country performance for models trained on set of
-    countries given.
-    """
-    n = len(country_names)
-    performance_matrix = np.zeros((trials, n, n))
-    data = load_data(country_paths, survey, dimension=None)
-    for trial in xrange(trials):
-        for in_idx in xrange(n):
-            performance_matrix[trial, :, in_idx] = compute_column(
-                country_names[in_idx], data, in_idx, dimension, k, points,
-                alpha_low, alpha_high)
-    performance_matrix = performance_matrix.mean(axis=0)
-    plot_model_performance(performance_matrix, country_names, cmap)
-    return np.around(performance_matrix, decimals=2)
-
-
-def compute_column(
-    country_name, data, in_idx, dimension, k, points, alpha_low,
-        alpha_high):
-    """
-    Evaluates in- and out-of-country performance for model trained on one
-    country.
-    """
-    in_data = data[in_idx]
-    out_data = list(data)
-    out_data.pop(in_idx)
-    if country_name == 'pooled':
-        r2s_in, r2s_out = evaluate_pooled_r2s(
-            out_data, dimension, k, points, alpha_low, alpha_high)
-    else:
-        r2s_in, r2s_out = evaluate_model_r2s(
-            in_data, out_data, dimension, k, points, alpha_low, alpha_high)
-    max_idx = np.argmax(r2s_in)
-    r2s = list(r2s_out[:, max_idx])
-    r2s.insert(in_idx, r2s_in[max_idx])
-    r2s.reverse()
-    r2s = np.array(r2s)
-    return r2s
-
-
-def evaluate_pooled_r2s(all_data, dimension, k, points, alpha_low, alpha_high):
-    """
-    Evaluates in- and out-of-country r2s for the pooled model.
-    """
-    X_all_full, y_all = [list(data) for data in zip(*all_data)]
-    alphas = np.logspace(alpha_low, alpha_high, points)
-    r2s_in = np.zeros((k, points))
-    r2s_out = np.zeros((len(all_data), k, points))
-    kf = []
-    for i in xrange(len(X_all_full)):
-        kf.append(list(cross_validation.KFold(
-            n=all_data[i][1].size, n_folds=k, shuffle=True)))
-    for fold in xrange(k):
-        X_train, X_test, y_train, y_test = split_pooled_data(
-            kf, fold, X_all_full, y_all)
-        X_train, X_test, X_all = reduce_and_scale_features(
-            X_train, X_test, X_all_full, dimension)
-        r2s_in, r2s_out = evaluate_alphas(
-            X_train, X_test, X_all, y_train, y_test, y_all, r2s_in, r2s_out,
-            fold, alphas)
-    return r2s_in.mean(axis=0), r2s_out.mean(axis=1)
-
-
-def split_pooled_data(kf, fold, X_all_full, y_all):
-    """
-    Splits pooled training and test data for each fold.
-    """
-    X_train = []
-    X_test = []
-    y_train = []
-    y_test = []
-    for i in xrange(len(kf)):
-        train_ind = kf[i][fold][0]
-        test_ind = kf[i][fold][1]
-        X_train.append(X_all_full[i][train_ind])
-        X_test.append(X_all_full[i][test_ind])
-        y_train.append(y_all[i][train_ind])
-        y_test.append(y_all[i][test_ind])
-    X_train = np.vstack(X_train)
-    X_test = np.vstack(X_test)
-    y_train = np.hstack(y_train)
-    y_test = np.hstack(y_test)
-    return X_train, X_test, y_train, y_test
-
-
-def evaluate_model_r2s(
-        in_data, out_data, dimension, k, points, alpha_low, alpha_high):
-    """
-    Evaluates in- and out-of-country r2s.
-    """
-    X, y = in_data
-    X_out_full, y_out = [list(data) for data in zip(*out_data)]
-    alphas = np.logspace(alpha_low, alpha_high, points)
-    r2s_in = np.zeros((k, points))
-    r2s_out = np.zeros((len(out_data), k, points))
-    kf = cross_validation.KFold(n=in_data[1].size, n_folds=k, shuffle=True)
-    fold = 0
-    for train_ind, test_ind in kf:
-        X_train, X_test = X[train_ind], X[test_ind]
-        y_train, y_test = y[train_ind], y[test_ind]
-        X_train, X_test, X_out = reduce_and_scale_features(
-            X_train, X_test, X_out_full, dimension)
-        r2s_in, r2s_out = evaluate_alphas(
-            X_train, X_test, X_out, y_train, y_test, y_out, r2s_in, r2s_out,
-            fold, alphas)
-        fold += 1
-    return r2s_in.mean(axis=0), r2s_out.mean(axis=1)
-
-
-def evaluate_alphas(
-    X_train, X_test, X_out, y_train, y_test, y_out, r2s_in, r2s_out, fold,
-        alphas):
-    """
-    Computes r2 for different regularization constants.
-    """
-    for idx, alpha in enumerate(alphas):
-        ridge = linear_model.Ridge(alpha=alpha)
-        ridge.fit(X_train.astype(np.float), y_train.astype(np.float))
-        r2s_in[fold, idx] = compute_r2(ridge, X_test, y_test)
-        for i in xrange(len(X_out)):
-            r2s_out[i, fold, idx] = compute_r2(ridge, X_out[i], y_out[i])
-    return r2s_in, r2s_out
-
-
-def reduce_and_scale_features(X_train, X_test, X_out_full, dimension):
-    """
-    Reduces dimension and scales in- and out-of-country features.
-    """
-    pca = PCA(n_components=dimension)
-    scaler = StandardScaler(with_mean=True, with_std=False)
-    X_train = scaler.fit_transform(pca.fit_transform(X_train))
-    X_test = scaler.transform(pca.transform(X_test))
-    X_out = []
-    for X in X_out_full:
-        X_out.append(scaler.transform(pca.transform(X)))
-    return X_train, X_test, X_out
-
-
-def compute_r2(model, X, y):
-    """
-    Computes model r2.
-    """
-    y_hat = model.predict(X)
-    r2 = stats.pearsonr(y, y_hat)[0] ** 2
-    return r2
-
-
-def plot_circles(data, ax, **kwargs):
-    """
-    Plots circles for r2 values.
-    """
-    M = np.array(data)
-    xy = np.indices(M.shape)[::-1].reshape(2, -1).T
-    w = np.abs(M).ravel()
-    h = np.abs(M).ravel()
-    a = 0
-    circles = EllipseCollection(
-        widths=w, heights=h, angles=a, units='x', offsets=xy,
-        transOffset=ax.transData, array=M.ravel(), **kwargs)
-    ax.add_collection(circles)
-    ax.set_xticks(np.arange(M.shape[1]))
-    ax.set_xticklabels(data.columns)
-    ax.set_yticks(np.arange(M.shape[0]))
-    ax.set_yticklabels(data.index, rotation=90)
-    return circles
-
-
-def plot_model_performance(data, country_names, cmap='Greens'):
-    """
-    Makes plot for in- and out-of-country model performance.
-    """
-    data = pd.DataFrame(np.flipud(data))
-    data.columns = [country.capitalize() for country in country_names]
-    data.index = [country.capitalize() for country in country_names]
-    sns.set_style('white')
-    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-    circles = plot_circles(data, ax=ax, cmap=cmap)
-    cb = fig.colorbar(circles)
-    cb.set_label('$r^2$', fontsize=16)
-    ax.margins(0.1)
-    plt.xlabel('Country trained on', fontsize=16)
-    plt.ylabel('Country evaluated on', fontsize=16)
-    plt.show()
-
 ############################
 ######### General ##########
 ############################
@@ -537,8 +250,8 @@ def load_country_lsms(country_path, exp=None):
         X = np.load(country_path + 'cluster_conv_features_' + exp + '.npy')
     else:
         X = np.load(country_path + 'cluster_conv_features.npy')
-    X_nl = np.load('/home/asamar/tile2vec/lsms/original_lsms/cluster_nightlights.npy').reshape(-1, 1)
-    X_tf = np.load('/home/asamar/tile2vec/lsms/original_lsms/cluster_conv_features.npy')
+    X_nl = np.load(country_path + 'cluster_nightlights.npy').reshape(-1, 1)
+    X_tf = np.load(country_path + 'cluster_conv_features_tf.npy')
     y = np.load(country_path + 'cluster_consumptions.npy')
     hhs = np.load(country_path + 'cluster_households.npy')
     images = np.load(country_path + 'cluster_image_counts.npy')
@@ -556,24 +269,6 @@ def load_country_lsms(country_path, exp=None):
     y = np.log(y)
     return X, X_nl, X_tf, y
 
-
-def load_country_dhs(country_path):
-    """
-    Loads data for one DHS country.
-    """
-    X = np.load(country_path + 'cluster_conv_features.npy')
-    X_nl = np.load(country_path + 'cluster_nightlights.npy').reshape(-1, 1)
-    y = np.load(country_path + 'cluster_assets.npy')
-    hhs = np.load(country_path + 'cluster_households.npy')
-    images = np.load(country_path + 'cluster_image_counts.npy')
-    # Filter out single households and <10 images
-    mask = np.logical_and((hhs >= 2), (images >= 10))
-    X = X[mask]
-    X_nl = X_nl[mask]
-    y = y[mask]
-    return X, X_nl, y
-
-
 def reduce_dimension(X, dimension):
     """
     Uses PCA to reduce dimensionality of features.
@@ -590,8 +285,6 @@ def load_and_reduce_country(country_path, survey, dimension):
     """
     if survey == 'lsms':
         X, _, y = load_country_lsms(country_path)
-    elif survey == 'dhs':
-        X, _, y = load_country_dhs(country_path)
     X = reduce_dimension(X, dimension)
     return X, y
 
---
+++ lsms/uganda_lsms/readme.txt
@@ -0,0 +1,32 @@
+This folder contains data from the Uganda 2011-2012 LSMS survey.
+Please contact asamar@alumni.stanford.edu to access
+
+cluster_consumptions.npy - Contains (642,) vector of cluster consumptions in 2011 USD per day.
+
+cluster_conv_features.npy - Contains (642,4096) array of cluster convolutional features.
+
+cluster_elevs.npy - Contains (642,) vector of average elevation.
+
+cluster_households.npy - Contains (642,) vector of the number of households in each cluster. There are 319 clusters with multiple households.
+
+cluster_image_counts.npy - Contains (642,) vector of cluster image counts. There are 5 clusters with no imagery, 637 with at least 1 image, 636 with at least 10 images, 621 with at least 50 images, 587 with at least 90 images, and 512 with at least 100 images.
+
+cluster_landscans.npy - Contains (642,) vector of the average Landscan population density estimate for each cluster.
+
+cluster_locs.npy - Contains (642,2) array of lat/lon locations for each cluster.
+
+cluster_metals.npy - Contains (642,) vector of the proportion of metal roofs in each cluster.
+
+cluster_nightlights.npy - Contains (642,) vector of the average nighttime light intensity of each cluster.
+
+cluster_pops.npy - Contains (642,) vector of the average distance to a population center of at least 20k people.
+
+cluster_roads.npy - Contains (642,) vector of the average distance to a major road.
+
+cluster_rooms.npy - Contains (642,) vector of the average number of rooms in each household of the cluster. This is a proxy for the size of the dwelling.
+
+cluster_urbans.npy - Contains (642,) vector of urban/rural marker for each cluster, with 1 being urban and 0 being rural.
+
+
+
+
