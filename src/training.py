852fece5a04e2f07fb6a71a414270c72ae1b12f0
Ubuntu <hopkilau@t2vMax.0u3hssfloelenakr4sfeydioig.bx.internal.cloudapp.net>
2020-01-22 07:03:14 +0000

    Added with torch.no_grad() to fix cuda out of memory error

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 import numpy as np
 from time import time
 from torch.autograd import Variable
+import torch
 from src.datasets import triplet_dataloader
 import sys
 
@@ -89,27 +90,29 @@ def validate_model(model, cuda, dataloader, optimizer, epoch, margin=1,
     """
     Validates a model using the provided dataloader.
     """
-    model.eval()
-    if t0 is None:
-        t0 = time.time()
-    sum_loss, sum_l_n, sum_l_d, sum_l_nd = (0, 0, 0, 0)
-    n_train, n_batches = len(dataloader.dataset), len(dataloader)
-    print_sum_loss = 0
-    for idx, triplets in enumerate(dataloader):
-        p, n, d = prep_triplets(triplets, cuda)
-        loss, l_n, l_d, l_nd = model.loss(p, n, d, margin=margin, l2=l2)
-        sum_loss += loss.item()  #data[0]
-        sum_l_n += l_n.item()  #data[0]
-        sum_l_d += l_d.item()  #data[0]
-        sum_l_nd += l_nd.item()  #data[0]
-    avg_loss = sum_loss / n_batches
-    avg_l_n = sum_l_n / n_batches
-    avg_l_d = sum_l_d / n_batches
-    avg_l_nd = sum_l_nd / n_batches
-    #print('Finished epoch {}: {:0.3f}s'.format(epoch, time()-t0))
-    #print('  Average loss: {:0.4f}'.format(avg_loss))
-    #print('  Average l_n: {:0.4f}'.format(avg_l_n))
-    #print('  Average l_d: {:0.4f}'.format(avg_l_d))
-    #print('  Average l_nd: {:0.4f}\n'.format(avg_l_nd))
-    print('Test Epoch {}: Loss {:0.4f}, Time {:0.3f}s'.format(epoch, avg_loss, time()-t0))
+    
+    with torch.no_grad():   # added per https://discuss.pytorch.org/t/out-of-memory-error-during-evaluation-but-training-works-fine/12274/4
+        model.eval()
+        if t0 is None:
+            t0 = time.time()
+        sum_loss, sum_l_n, sum_l_d, sum_l_nd = (0, 0, 0, 0)
+        n_train, n_batches = len(dataloader.dataset), len(dataloader)
+        print_sum_loss = 0
+        for idx, triplets in enumerate(dataloader):
+            p, n, d = prep_triplets(triplets, cuda)
+            loss, l_n, l_d, l_nd = model.loss(p, n, d, margin=margin, l2=l2)
+            sum_loss += loss.item()  #data[0]
+            sum_l_n += l_n.item()  #data[0]
+            sum_l_d += l_d.item()  #data[0]
+            sum_l_nd += l_nd.item()  #data[0]
+        avg_loss = sum_loss / n_batches
+        avg_l_n = sum_l_n / n_batches
+        avg_l_d = sum_l_d / n_batches
+        avg_l_nd = sum_l_nd / n_batches
+        #print('Finished epoch {}: {:0.3f}s'.format(epoch, time()-t0))
+        #print('  Average loss: {:0.4f}'.format(avg_loss))
+        #print('  Average l_n: {:0.4f}'.format(avg_l_n))
+        #print('  Average l_d: {:0.4f}'.format(avg_l_d))
+        #print('  Average l_nd: {:0.4f}\n'.format(avg_l_nd))
+        print('Test Epoch {}: Loss {:0.4f}, Time {:0.3f}s'.format(epoch, avg_loss, time()-t0))
     return avg_loss #(avg_loss, avg_l_n, avg_l_d, avg_l_nd)
