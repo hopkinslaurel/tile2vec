4b405cd077f0008f566ee5f07ec2732ff84b1572
Ubuntu <hopkilau@t2vMax.0u3hssfloelenakr4sfeydioig.bx.internal.cloudapp.net>
2020-03-09 02:31:08 +0000

    added function to get label from triplet index

--- species_labels.py
+++ species_labels.py
@@ -1,6 +1,7 @@
 import csv
+import numpy as np
 
-def get_records():
+def import_records():
     records = {}
     with open('OR_2011_synthetic_responses.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -9,5 +10,14 @@ def get_records():
                 'forest_no_frag_1':int(row['response_forest_no_frag']), 'desert':int(row['response_desert']), \
                 'desert_no_frag':int(row['response_desert_no_frag']), 'frag':int(row['response_frag']), \
                 'interaction':int(row['response_interaction'])}
-            
     return records
+
+def get_records(triplet_idx, species):
+    records = import_records()
+    y = np.zeros(len(triplet_idx))
+    idx = 0
+    for triplet in triplet_idx:
+        y[idx] = records[str(triplet)][species]
+        idx += 1
+
+    return y
